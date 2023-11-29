2a1f329600f243b066d66d8b21b69ae927203ee1
Yasuo Takeichi <takeichi@ap.eng.osaka-u.ac.jp>
2023-11-29 11:13:47 +0900

    Replaced 'String.valueOf'

--- src/Create_ColorBar.java
+++ src/Create_ColorBar.java
@@ -6,28 +6,28 @@ public class Create_ColorBar implements PlugIn {
 	ImagePlus impCurrent;
 	ImagePlus impBar;
 	ImagePlus impFlat;
-	
+
 	public static int width = 32;
 	public static int height = 256;
-	
+
 	public void run(String arg) {
 		impCurrent = WindowManager.getCurrentImage();
-		if(impCurrent == null) {
+		if (impCurrent == null) {
 			IJ.error("There are no images open.");
 			return;
 		}
-		if(impCurrent.getBitDepth() == 24) {
+		if (impCurrent.getBitDepth() == 24) {
 			IJ.error("Current image is not a single channel (value) image.");
 			return;
 		}
 		IJ.log("Display range of current image:");
-		IJ.log("min = " + String.valueOf(impCurrent.getDisplayRangeMin()) + ", max = " + String.valueOf(impCurrent.getDisplayRangeMax()));
-		
+		IJ.log("min = " + impCurrent.getDisplayRangeMin() + ", max = " + impCurrent.getDisplayRangeMax());
+
 		impBar = NewImage.createImage("ColorBar", width, height, 1, 32, NewImage.FILL_BLACK);
-		float[] pixels = (float[])impBar.getProcessor().getPixels();
-		for(int i = 0; i < height; i++) {
-			for(int j = 0; j < width; j++) {
-				pixels[width * i + j] = (float)(height - 1 - i);
+		float[] pixels = (float[]) impBar.getProcessor().getPixels();
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				pixels[width * i + j] = (float) (height - 1 - i);
 			}
 		}
 		impBar.setLut(impCurrent.getProcessor().getLut());
